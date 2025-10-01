cfc94a7d6abc53e3beb2387472620b2e836a4f12
Agasthi98 <sankalana98@gmail.com>
2025-10-01 14:18:49 +0530

    impleemnted a method to reverse a string

--- src/main/java/com/example/backendtemplate/service/TestService.java
+++ src/main/java/com/example/backendtemplate/service/TestService.java
@@ -2,10 +2,10 @@ package com.example.backendtemplate.service;
 
 import com.example.backendtemplate.model.response.BaseDetailsResponse;
 
-import java.util.HashMap;
 
 public interface TestService {
 
     BaseDetailsResponse<String> getBirthDate(String nic);
+    BaseDetailsResponse<String> reverseString(String text);
 
 }
--- src/main/java/com/example/backendtemplate/service/impl/TestServiceImpl.java
+++ src/main/java/com/example/backendtemplate/service/impl/TestServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.backendtemplate.model.response.BaseDetailsResponse;
 import com.example.backendtemplate.service.TestService;
 import com.example.backendtemplate.util.NumberUtil;
 import com.example.backendtemplate.util.ResponseUtil;
+import com.example.backendtemplate.util.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,17 @@ public class TestServiceImpl implements TestService {
                 .title(ResponseUtil.SUCCESS)
                 .message("Birth date fetched successfully")
                 .data(NumberUtil.getBirthDateFromNIC(nic))
+                .build();
+    }
+
+    @Override
+    public BaseDetailsResponse<String> reverseString(String text) {
+        String t = StringUtils.reverseString(text);
+        return BaseDetailsResponse.<String>builder()
+                .code(ResponseUtil.SUCCESS_CODE)
+                .title(ResponseUtil.SUCCESS)
+                .message(ResponseUtil.SUCCESS)
+                .data(t)
                 .build();
     }
 }
